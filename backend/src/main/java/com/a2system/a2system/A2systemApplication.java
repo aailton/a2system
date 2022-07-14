d2d528e057f680e51f00c91073bdc208dcfb9c64
Ailton Alves <alvs.ailton@gmail.com>
2022-07-14 19:54:26 -0300

    Project created

---
+++ backend/src/main/java/com/a2system/a2system/A2systemApplication.java
@@ -0,0 +1,13 @@
+package com.a2system.a2system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2systemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A2systemApplication.class, args);
+	}
+
+}
