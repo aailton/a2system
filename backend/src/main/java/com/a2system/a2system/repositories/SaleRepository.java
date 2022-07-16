0fa67a4830aa286ccd466ef53b57fa5ea00acf26
Ailton Alves <alvs.ailton@gmail.com>
2022-07-16 20:01:24 -0300

    API test

---
+++ backend/src/main/java/com/a2system/a2system/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.a2system.a2system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.a2system.a2system.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
